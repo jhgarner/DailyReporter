cca83358cf8ff7d4bebe41c94e7ef6091e85b963
Jack Garner <jacksonhgarner@outlook.com>
2026-06-07 12:22:19 -0700

    Fix app.nix to be deterministic

--- app.nix
+++ app.nix
@@ -1,31 +1,108 @@
-{ mkDerivation, aeson, base, bytestring, deriving-aeson
-, fast-tagsoup, feed, generic-data, hashable, hspec, hspec-discover
-, http-api-data, indexed-traversable, lib, modern-uri, monad-logger
-, mtl, PyF, recursion-schemes, req, scalpel-core, tagsoup, text
-, theseus, time, unliftio, webdriver
+{
+  mkDerivation,
+  aeson,
+  base,
+  bytestring,
+  deriving-aeson,
+  fast-tagsoup,
+  feed,
+  generic-data,
+  hashable,
+  hspec,
+  hspec-discover,
+  http-api-data,
+  indexed-traversable,
+  lib,
+  modern-uri,
+  monad-logger,
+  mtl,
+  PyF,
+  recursion-schemes,
+  req,
+  scalpel-core,
+  tagsoup,
+  text,
+  theseus,
+  time,
+  unliftio,
+  webdriver,
 }:
 mkDerivation {
   pname = "DailyReporter";
   version = "0.1.0.0";
-  src = ./.;
+  src = lib.cleanSource ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base bytestring deriving-aeson fast-tagsoup feed generic-data
-    hashable http-api-data indexed-traversable modern-uri monad-logger
-    mtl PyF recursion-schemes req scalpel-core tagsoup text theseus
-    time unliftio webdriver
+    aeson
+    base
+    bytestring
+    deriving-aeson
+    fast-tagsoup
+    feed
+    generic-data
+    hashable
+    http-api-data
+    indexed-traversable
+    modern-uri
+    monad-logger
+    mtl
+    PyF
+    recursion-schemes
+    req
+    scalpel-core
+    tagsoup
+    text
+    theseus
+    time
+    unliftio
+    webdriver
   ];
   executableHaskellDepends = [
-    aeson base bytestring deriving-aeson fast-tagsoup feed generic-data
-    hashable http-api-data indexed-traversable modern-uri mtl PyF
-    recursion-schemes req scalpel-core tagsoup text theseus time
+    aeson
+    base
+    bytestring
+    deriving-aeson
+    fast-tagsoup
+    feed
+    generic-data
+    hashable
+    http-api-data
+    indexed-traversable
+    modern-uri
+    mtl
+    PyF
+    recursion-schemes
+    req
+    scalpel-core
+    tagsoup
+    text
+    theseus
+    time
     unliftio
   ];
   testHaskellDepends = [
-    aeson base bytestring deriving-aeson fast-tagsoup feed generic-data
-    hashable hspec http-api-data indexed-traversable modern-uri mtl PyF
-    recursion-schemes req scalpel-core tagsoup text theseus time
+    aeson
+    base
+    bytestring
+    deriving-aeson
+    fast-tagsoup
+    feed
+    generic-data
+    hashable
+    hspec
+    http-api-data
+    indexed-traversable
+    modern-uri
+    mtl
+    PyF
+    recursion-schemes
+    req
+    scalpel-core
+    tagsoup
+    text
+    theseus
+    time
     unliftio
   ];
   testToolDepends = [ hspec-discover ];
